7d9b3a3b0ef0c5d7de299a51fd6f2bdf345b74ab
andy <andy@andydote.co.uk>
2016-08-15 16:24:36 +0100

    tests: add scratchpad

---
+++ NRaft.Tests/Scratchpad.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace NRaft.Tests
+{
+	public class Scratchpad
+	{
+		private readonly ITestOutputHelper _output;
+
+		public Scratchpad(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Fact]
+		public void When_testing_something()
+		{
+
+		}
+	}
+}
