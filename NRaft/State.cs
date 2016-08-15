c433546bb2d8aa9e19cd0ca0952c7d1cd28fe4ce
andy <andy@andydote.co.uk>
2016-08-15 16:19:16 +0100

    initial version

---
+++ NRaft/State.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace NRaft
+{
+	public class State
+	{
+		private readonly int _nodeID;
+
+		//need to be persistent store
+		private int _currentTerm;
+		private int? _votedFor;
+		private List<LogEntry> _log;
+
+		//only in memory
+		private int _commitIndex;
+		private int _lastApplied;
+
+		//leader-only state - perhaps refactor to leaderState
+		private int _nextIndex;
+		private int _matchIndex;
+
+		public State(int nodeID)
+		{
+			_nodeID = nodeID;
+			_currentTerm = 0;
+			_votedFor = null;
+			_log = new List<LogEntry>();
+
+			_commitIndex = 0;
+			_lastApplied = 0;
+		}
+	}
+
+	public class LogEntry
+	{
+		public string Command { get; set; }
+		public int Term { get; set; }
+	}
+
+	public class AppendEntriesRpc
+	{
+		public int Term { get; set; }
+		public int LeaderID { get; set; }
+
+		public int PreviousLogIndex { get; set; }
+		public int PreviousLogTerm { get; set; }
+
+		public LogEntry[] Entries { get; set; }
+		public int LeaderCommit { get; set; }
+	}
+
+	public class AppendEntriesResponse
+	{
+		public int Term { get; set; }
+		public bool Success { get; set; }
+	}
+}
