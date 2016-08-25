f6b33c0ea055d6556213a8ca550326b194a31478
andy <andy@andydote.co.uk>
2016-08-25 09:21:43 +0100

    quorum: add function for generating all possible quorums

---
+++ NRaft.Tests/Infrastructure/QuorumTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using NRaft.Infrastructure;
+using Shouldly;
+using Xunit;
+
+namespace NRaft.Tests.Infrastructure
+{
+	public class QuorumTests
+	{
+		[Theory]
+		[InlineData("10", "10")]
+		[InlineData("10,20", "10,20")]
+		[InlineData("10,20,30", "10,20 - 10,30 - 20,30 - 10,20,30")]
+		public void When(string input, string result)
+		{
+			var nodes = input
+				.Split(',')
+				.Select(n => Convert.ToInt32(n))
+				.ToArray();
+
+			var expected = result
+				.Split(new[] { " - " },StringSplitOptions.None)
+				.Select(line => line.Split(',')
+				.Select(n => Convert.ToInt32(n)));
+
+			Quorum
+				.GenerateAllPossibilities(nodes)
+				.ShouldBe(expected);
+		}
+	}
+}
---
+++ NRaft/Infrastructure/Quorum.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NRaft.Infrastructure
+{
+	public static class Quorum
+	{
+		public static IEnumerable<int[]> GenerateAllPossibilities(int[] nodeIDs)
+		{
+			var ps = PowerSet(nodeIDs);
+			var quorum = ps.Where(i => i.Length * 2 > nodeIDs.Length);
+
+			return quorum;
+		}
+
+		private static List<T[]> PowerSet<T>(T[] input)
+		{
+			var n = input.Length;
+			var powerSetCount = 1 << n; // Power set contains 2^N subsets.
+
+			var ans = new List<T[]>();
+
+			for (var setMask = 0; setMask < powerSetCount; setMask++)
+			{
+				var s = new List<T>();
+				for (var i = 0; i < n; i++)
+				{
+					// Checking whether i'th element of input collection should go to the current subset.
+					if ((setMask & (1 << i)) > 0)
+					{
+						s.Add(input[i]);
+					}
+				}
+				ans.Add(s.ToArray());
+			}
+
+			return ans;
+
+		}
+	}
+}
