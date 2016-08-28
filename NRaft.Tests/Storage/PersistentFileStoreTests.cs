666bf295cbb699554802a4fd2764c105fdf3b212
andy <andy@andydote.co.uk>
2016-08-28 22:02:09 +0100

    persistentFileStore: basic implementation and test stub

---
+++ NRaft.Tests/Storage/PersistentFileStoreTests.cs
@@ -0,0 +1,14 @@
+﻿using NRaft.Storage;
+
+namespace NRaft.Tests.Storage
+{
+	public class PersistentFileStoreTests
+	{
+		private readonly PersistentFileStore _store;
+
+		public PersistentFileStoreTests()
+		{
+			_store = new PersistentFileStore();
+		}
+	}
+}
---
+++ NRaft/Storage/PersistentFileStore.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace NRaft.Storage
+{
+	public class PersistentFileStore : IStore
+	{
+		public int CurrentTerm => _store.Value.CurrentTerm;
+		public int? VotedFor => _store.Value.VotedFor;
+		public LogEntry[] Log => _store.Value.Log;
+
+		private readonly string _path;
+		private readonly Lazy<Dto> _store;
+
+		public PersistentFileStore(string path)
+		{
+			_path = path;
+			_store = new Lazy<Dto>(FromDisk);
+		}
+
+		public void Write(Action<IStoreWriter> apply)
+		{
+			apply(_store.Value);
+			Write();
+		}
+
+		private Dto FromDisk()
+		{
+			return JsonConvert.DeserializeObject<Dto>(File.ReadAllText(_path));
+		}
+
+		private void Write()
+		{
+			File.WriteAllText(_path, JsonConvert.SerializeObject(_store.Value));
+		}
+
+		private class Dto : IStoreWriter
+		{
+			public int CurrentTerm { get; set; }
+			public int? VotedFor { get; set; }
+			public LogEntry[] Log { get; set; }
+		}
+	}
+}
