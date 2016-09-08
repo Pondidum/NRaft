362cbb70f853a1829fbed8f59fcf518914a4fbd7
andy <andy@andydote.co.uk>
2016-09-08 23:36:25 +0300

    states: define rough transition and setup methods

--- NRaft/States/States.cs
+++ NRaft/States/States.cs
@@ -1,17 +1,60 @@
-﻿namespace NRaft.States
+﻿using System;
+
+namespace NRaft.States
 {
-	public class Follower
+
+	public class NodeInfo
 	{
 		
+	}
+
+	public class Follower
+	{
+		private readonly NodeInfo _info;
+
+		public Follower(NodeInfo info)
+		{
+			_info = info;
+		}
+
+		public Candidate BecomeCandidate()
+		{
+			throw new NotImplementedException();
+		}
 	}
 
 	public class Candidate
 	{
-		
+		private readonly NodeInfo _info;
+
+		public Candidate(NodeInfo info)
+		{
+			_info = info;
+		}
+
+		public Follower BecomeFollower()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Leader BecomeLeader()
+		{
+			throw new NotImplementedException();
+		}
 	}
 
 	public class Leader
 	{
-		
+		private readonly NodeInfo _info;
+
+		public Leader(NodeInfo info)
+		{
+			_info = info;
+		}
+
+		public Follower BecomeFollower()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
