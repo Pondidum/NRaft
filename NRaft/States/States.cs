fbfc744d868d63a4d23efa6bbd502c4a28f097ee
andy <andy@andydote.co.uk>
2016-09-09 20:45:41 +0300

    states: add coordinator (eh, name?) to handle state transitions

--- NRaft/States/States.cs
+++ NRaft/States/States.cs
@@ -1,11 +1,32 @@
 ﻿using System;
+using NRaft.Infrastructure;
 
 namespace NRaft.States
 {
 
+	public class Coordinator
+	{
+		private readonly IPulseable _heart;
+		private State _state;
+
+		public Coordinator(IClock clock)
+		{
+			_heart = clock.CreatePulseMonitor(TimeSpan.FromMilliseconds(350), OnHeartbeatElapsed); //should be random...
+			_state = new Follower(new NodeInfo());
+		}
+
+		//I wanted to call this OnHeartFailure...
+		private void OnHeartbeatElapsed()
+		{
+			var follower = _state as Follower;
+
+			if (follower != null)
+				_state = follower.BecomeCandidate();
+		}
+	}
+
 	public class NodeInfo
 	{
-		
 	}
 
 	public class State { }
