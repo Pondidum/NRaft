1d4de902629b46e9dc65b2da9ffc5ec38c14ba0c
andy <andy@andydote.co.uk>
2016-09-21 21:26:08 +0300

    timings: add interfaces for new timers

---
+++ NRaft/Timing/IHeart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NRaft.Timing
+{
+	public interface IHeart
+	{
+		void StartPulsing(TimeSpan interval);
+		void StopPulsing();
+
+		void ConnectTo(Action onHeartbeat);
+	}
+
+	public interface IElectionTimeout
+	{
+		void StartElection(TimeSpan duration);
+
+		void ConnectTo(Action onElectionOver);
+	}
+
+	public interface IPulseMonitor
+	{
+		void StartMonitoring(TimeSpan duration);
+		void StopMonitoring();
+
+		void ConnectTo(Action onPulseLost);
+	}
+}
