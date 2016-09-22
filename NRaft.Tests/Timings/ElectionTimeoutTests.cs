ec04e57dd1f76c424ab8a1a68c22624e8125210f
andy <andy@andydote.co.uk>
2016-09-22 19:18:04 +0300

    timing: implement electionTimeout

--- NRaft.Tests/Timings/ElectionTimeoutTests.cs
+++ NRaft.Tests/Timings/ElectionTimeoutTests.cs
@@ -12,7 +12,7 @@ namespace NRaft.Tests.Timings
 
 		public ElectionTimeoutTests()
 		{
-			//_election = new ElectionTimeout();
+			_election = new ElectionTimeout();
 		}
 
 		[Fact]
@@ -22,15 +22,22 @@ namespace NRaft.Tests.Timings
 		}
 
 		[Fact]
-		public async Task When_started_twice()
+		public async Task When_started_twice_the_second_election_is_the_one_which_runs()
 		{
 			var endings = 0;
 			_election.ConnectTo(() => endings++);
 
 			_election.StartElection(TimeSpan.FromMilliseconds(50));
+
+			await Task.Delay(25);
+
 			_election.StartElection(TimeSpan.FromMilliseconds(50));
 
-			await Task.Delay(75);
+			await Task.Delay(40);
+
+			endings.ShouldBe(0);
+
+			await Task.Delay(25);
 
 			endings.ShouldBe(1);
 		}
---
+++ NRaft/Timing/ElectionTimeout.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NRaft.Timing
+{
+	public class ElectionTimeout : IElectionTimeout
+	{
+		private TimeSpan _duration;
+		private Action _onElectionOver;
+		private Task _timeout;
+		private CancellationTokenSource _cancellation;
+
+		public void StartElection(TimeSpan duration)
+		{
+			if (_onElectionOver == null)
+				throw new InvalidOperationException(".ConnectTo(Action onElectionOver) must have been called at least once.");
+
+			_duration = duration;
+
+			if (_cancellation != null)
+			{
+				try
+				{
+					_cancellation.Cancel();
+					_timeout.Wait();
+				}
+				catch (AggregateException)
+				{
+				}
+
+				_cancellation.Dispose();
+				_timeout.Dispose();
+			}
+
+			_cancellation = new CancellationTokenSource();
+			_timeout = Task.Run(() => Election(), _cancellation.Token);
+		}
+
+		public void ConnectTo(Action onElectionOver)
+		{
+			_onElectionOver = onElectionOver;
+		}
+
+		private void Election()
+		{
+			Task.Delay(_duration, _cancellation.Token).Wait(_cancellation.Token);
+
+			_onElectionOver();
+		}
+	}
+}
